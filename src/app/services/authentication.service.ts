561b1c291e73c7e3543db7b5e29cec919f2404de
touzaelhassan <touzaelhassan@gmail.com>
2023-01-02 11:41:05 +0100

    Creating user service functionalities

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -9,50 +9,50 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({ providedIn: 'root'})
 export class AuthenticationService {
 
-  private host = environment.APIEnpointsURL;
-  private token: any;
-  private loggedInUsername: any;
-  private jwtHelper = new JwtHelperService();
+    private host = environment.APIEnpointsURL;
+    private token: any;
+    private loggedInUsername: any;
+    private jwtHelper = new JwtHelperService();
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  public register(user: User) :Observable<User | HttpErrorResponse> {
-      return this.http.post<User | HttpErrorResponse>(`${this.host}/user/register`, user);
-  }
+    public register(user: User) :Observable<User | HttpErrorResponse> {
+        return this.http.post<User | HttpErrorResponse>(`${this.host}/user/register`, user);
+    }
 
-  public login(user: User) :Observable<HttpResponse<any> | HttpErrorResponse> {
-      return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe:'response'});
-  }
+    public login(user: User) :Observable<HttpResponse<any> | HttpErrorResponse> {
+        return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe:'response'});
+    }
 
-  public logout() : void {
-      this.token = null;
-      this.loggedInUsername = null;
-      localStorage.removeItem('user');
-      localStorage.removeItem('token');
-      localStorage.removeItem('users');
-  }
+    public logout() : void {
+        this.token = null;
+        this.loggedInUsername = null;
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        localStorage.removeItem('users');
+    }
 
-  public isUserLoggedIn() : boolean {
+    public isUserLoggedIn() : boolean {
 
-      this.loadToken();
+        this.loadToken();
 
-      if (this.token != null && this.token !== '') {
-          if (this.jwtHelper.decodeToken(this.token).sub != null || '') {
-              if (!this.jwtHelper.isTokenExpired(this.token)) {
-                  this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
-                  return true; 
-              }
-          }
-      }else{
-          this.logout();
-          return false;
-      }
-  }
+        if (this.token != null && this.token !== '') {
+            if (this.jwtHelper.decodeToken(this.token).sub != null || '') {
+                if (!this.jwtHelper.isTokenExpired(this.token)) {
+                    this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
+                    return true; 
+                }
+            }
+        }else{
+            this.logout();
+            return false;
+        }
+    }
 
-  public saveTokenInLocalStorage(token : string): void { this.token = token; localStorage.setItem('token', token); }
-  public loadToken(): void { this.token = localStorage.getItem('token'); }
-  public getToken(): string { return this.token; }
-  public saveUserInLocalStorage(user: User): void { localStorage.setItem('user', JSON.stringify(user)); }
-  public getUserFromLocalStorage(): User { return JSON.parse(localStorage.getItem('user')); }
+    public saveTokenInLocalStorage(token : string): void { this.token = token; localStorage.setItem('token', token); }
+    public loadToken(): void { this.token = localStorage.getItem('token'); }
+    public getToken(): string { return this.token; }
+    public saveUserInLocalStorage(user: User): void { localStorage.setItem('user', JSON.stringify(user)); }
+    public getUserFromLocalStorage(): User { return JSON.parse(localStorage.getItem('user')); }
 
 }
